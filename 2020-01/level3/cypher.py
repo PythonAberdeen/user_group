7eab5a3bba95004a5c5fd0bcffc7c9f3832e4ce5
Enzo <b.chybowski@gmail.com>
2020-01-08 19:42:46 +0000

    add chunk splitting

--- 2020-01/level3/cypher.py
+++ 2020-01/level3/cypher.py
@@ -3,8 +3,9 @@ alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'
 alphabet_len = len(alphabet)
 
 
-def cipher(text, number):
+def cipher(text, number, chunk):
     new_text = ""
+    i = 0;
     for letter in text:
         try:
             position = alphabet.index(letter.capitalize())
@@ -14,12 +15,19 @@ def cipher(text, number):
         if new_position > alphabet_len:
             new_position = number - (alphabet_len - position)
         new_text += alphabet[new_position]
+        i += 1
+        if i == 4:
+            new_text += ' '
+            i = 0
     return new_text
 
 
 def decipher(text, number):
     new_text = ""
     for letter in text:
+        if letter == ' ':
+            new_text += letter
+            continue
         try:
             position = alphabet.index(letter)
             new_position = position - number
@@ -31,5 +39,5 @@ def decipher(text, number):
     return new_text
 
 
-print(cipher('Python Aberdeen!', 5))
+print(cipher('Python Aberdeen!', 5, 4))
 print(decipher('UDYM TSFG JWIJ JSXI', 5))
