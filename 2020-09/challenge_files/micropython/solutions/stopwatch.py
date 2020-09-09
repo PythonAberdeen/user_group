cad2b6ce804cc7854c064765f44444789b7f9041
Robert McWilliam <rmcw@allmail.net>
2020-09-09 21:14:00 +0100

    Add challenge solutions.

---
+++ 2020-09/challenge_files/micropython/solutions/stopwatch.py
@@ -0,0 +1,27 @@
+from machine import Pin, I2C
+from time import sleep
+from ht16k33 import ht16k33_seg
+
+button = Pin(5, Pin.IN)
+
+prev = 0
+i2c = I2C(sda=Pin(18), scl=Pin(19), freq=100000)
+d = ht16k33_seg.Seg7x4(i2c)
+i = 0
+
+while True:
+    cur = button.value()
+    d.text("{:04d}".format(i))
+    d.show() 
+    
+    if cur != prev:
+        if cur:
+            print("Pressed")
+            sleep(5)
+        else:
+            print("Released")
+            i = 0
+        prev = cur
+    i += 1  
+    sleep(1)
+        
