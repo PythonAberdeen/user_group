37749e17d11b7b03c5d122043845a2ab5240f8f9
watty62 <ianwatt@gmail.com>
2024-06-12 19:49:20 +0100

    add IW.py

---
+++ 2024/2024-06/advanced/solutions/iw.py
@@ -0,0 +1,65 @@
+#Solution from ChatGPT4.0
+def is_valid(board, row, col, num):
+    # Check if num is not in the given row
+    for x in range(9):
+        if board[row][x] == num:
+            return False
+
+    # Check if num is not in the given column
+    for x in range(9):
+        if board[x][col] == num:
+            return False
+
+    # Check if num is not in the given 3x3 box
+    start_row = row - row % 3
+    start_col = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    empty = find_empty_location(board)
+    if not empty:
+        return True  # No empty space left, puzzle solved
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0  # Undo the move
+
+    return False
+
+def find_empty_location(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+# Example usage
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(sudoku_board):
+    for row in sudoku_board:
+        print(row)
+else:
+    print("No solution exists")
+
